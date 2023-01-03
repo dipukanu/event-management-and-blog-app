b2b08e4588224b44bad744b59a1e6ecf14b2fe39
dipukanu96 <dipu.kanu96@healthosbd.com>
2023-01-03 21:33:57 +0600

    Added csrf trusted origins in settings

--- EventManagementSystem/settings.py
+++ EventManagementSystem/settings.py
@@ -137,9 +137,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# CSRF_TRUSTED_ORIGINS = [
-#     ''
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://event-management-and-blog-app-production.up.railway.app'
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
